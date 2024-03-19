5f650bfe52e7c8f47893ab1279cd907538b85823
StudioRecinfo <studio@recinfotech.com>
2024-03-19 13:20:35 +0530

    -Added Animations

---
+++ Assets/IOSNotification.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class IOSNotification : MonoBehaviour
+{
+    public static void RegisterForNotif()
+    {
+
+    }
+
+    public static void ScheduleNotification(int longDelay, bool repeat, string title, string message)
+    {
+
+    }
+
+    public static void CancelNotifications()
+    {
+
+    }
+}
